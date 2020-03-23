d572922b3c4526ef1062cc40fc7d19a957d95eba
tadeifelipe <felipe_santostadei@hotmail.com>
2020-03-23 17:52:10 -0300

    change default

--- app.js
+++ app.js
@@ -9,15 +9,18 @@ class Despesa{
     }
 }
 
-
 function cadastrarDespesa(){
-    let ano = document.getElementById("ano");
-    let mes = document.getElementById("mes");
-    let dia = document.getElementById("dia");
-    let tipo = document.getElementById("tipo");
-    let descricao = document.getElementById("descricao");
-    let valor = document.getElementById("valor");
+    let ano = document.getElementById('ano');
+    let mes = document.getElementById('mes');
+    let dia = document.getElementById('dia');
+    let tipo = document.getElementById('tipo');
+    let descricao = document.getElementById('descricao');
+    let valor = document.getElementById('valor');
 
     let despesa = new Despesa(ano.value,mes.value,dia.value,tipo.value,descricao.value,valor.value);
-    console.log(despesa);
+    gravar(despesa);
+}
+
+function gravar(d){
+    localStorage.setItem('despesa',JSON.stringify(d));
 }
