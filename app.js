dec86d10286bc991e4f2e282ce7ed911101dfb92
tadeifelipe <felipe_santostadei@hotmail.com>
2020-03-25 08:58:10 -0300

    change default

--- app.js
+++ app.js
@@ -7,7 +7,41 @@ class Despesa{
         this.descricao = descricao;
         this.valor = valor;
     }
+
+    validarDados(){
+        for(let i in this){
+            if(this[i] == undefined || this[i] == '' || this[i] == null)
+                return false
+        }
+        return true;
+    }
 }
+
+class Bd {
+
+    constructor(){
+        let id = localStorage.getItem('id');
+        
+        if(id === null){
+            localStorage.setItem('id',0)
+        }
+    }
+
+    getProximoId(){
+        let proximoId = localStorage.getItem('id');
+        return parseInt(proximoId) + 1;
+    }
+
+    gravar(d){
+        let id = this.getProximoId();
+        
+        localStorage.setItem(id,JSON.stringify(d));
+        
+        localStorage.setItem('id',id);
+    }
+}
+
+let bd = new Bd();
 
 function cadastrarDespesa(){
     let ano = document.getElementById('ano');
@@ -18,9 +52,13 @@ function cadastrarDespesa(){
     let valor = document.getElementById('valor');
 
     let despesa = new Despesa(ano.value,mes.value,dia.value,tipo.value,descricao.value,valor.value);
-    gravar(despesa);
+    
+    if(despesa.validarDados()){
+       bd.gravar(despesa);
+       $('#sucessoGravacao').modal('show');
+    } 
+    else{
+        $('#erroGravacao').modal('show');
+    }
 }
 
-function gravar(d){
-    localStorage.setItem('despesa',JSON.stringify(d));
-}
